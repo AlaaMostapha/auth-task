572942ff88776dc4cfb040ee3489efeb17444d79
AlaaMostapha <alaamoustapha21@gmail.com>
2020-12-20 15:10:49 +0200

    install  react form hook , resolvers and it's error msg

---
+++ src/pages/Register/Register-copy.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import FormikControl from "../../components/Form/FormikControl";
+import CreateButton from "../../components/Button/Button";
+import { useDispatch, useSelector } from "react-redux";
+import * as actions from "../../redux/actions/register";
+import { ToastContainer } from "react-toastify";
+
+function Register(props) {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.registerReducer.loading);
+  console.log("loading", loading);
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    phoneNumber: "",
+  };
+  const validationSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    email: Yup.string()
+      .required("Email is required")
+      .email("Invalid Email format"),
+    password: Yup.string().required("Password is required").min(8),
+    confirmPassword: Yup.string()
+      .required("Confirm Password is required")
+      .oneOf([Yup.ref("password"), ""], "Passwords must be matched"),
+    phoneNumber: Yup.number().required("Phone number is required"),
+  });
+
+  const onSubmit = (values, onSubmitProps) => {
+    // console.log("form state", values);
+    dispatch(actions.Registiration(values));
+    onSubmitProps.setSubmitting(false);
+  };
+  return (
+    <>
+      <ToastContainer limit={2}></ToastContainer>
+      <h1>Registiration</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {(formik) => {
+          return (
+            <Form>
+              <FormikControl
+                control="material-ui-input"
+                type="text"
+                name="username"
+                label="Username"
+                autoComplete="off"
+                id="userName"
+              />
+              <FormikControl
+                control="material-ui-input"
+                type="email"
+                name="email"
+                label="Email"
+                autoComplete="off"
+                id="email"
+              />
+              <FormikControl
+                control="material-ui-input"
+                type="password"
+                name="password"
+                label="Password"
+                autoComplete="off"
+                id="password"
+              />
+              <FormikControl
+                control="material-ui-input"
+                type="password"
+                name="confirmPassword"
+                label="Confirm Password"
+                autoComplete="off"
+                id="confirmPassword"
+              />
+              <FormikControl
+                control="material-ui-input"
+                type="text"
+                name="phoneNumber"
+                label="Phone Number"
+                autoComplete="off"
+                id="phoneNumber"
+              />
+              <CreateButton
+                id="registerBtn"
+                variant="contained"
+                type="submit"
+                text="Register"
+                color="secondary"
+                disabled={!formik.isValid || formik.isSubmitting}
+              />
+            </Form>
+          );
+        }}
+      </Formik>
+    </>
+  );
+}
+
+export default Register;
