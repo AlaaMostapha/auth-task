b5e6ee9294d5a66744367258329f817700434c24
AlaaMostapha <alaamoustapha21@gmail.com>
2020-12-20 15:47:51 +0200

    change formik to react hook form in login and register

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,85 +1,65 @@
 import React from "react";
 import * as Yup from "yup";
-import { Formik, Form } from "formik";
-import FormikControl from "../../components/Form/FormikControl";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Input from "../../components/reactHookForm/input";
 import CreateButton from "../../components/Button/Button";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import * as actions from "../../redux/actions/login";
 import { ToastContainer } from "react-toastify";
-import * as passwordActions from "../../redux/actions/password";
 function Login(props) {
   const dispatch = useDispatch();
 
-  const forgetPassword = (user) => {
-    if (user) {
-      dispatch(passwordActions.forgetPassword(user));
-    }
-  };
-  const initialValues = {
+  const defaultValues = {
     email: "",
     password: "",
   };
-  const validationSchema = Yup.object({
+  const schema = Yup.object({
     email: Yup.string()
       .required("Email is required")
       .email("Invalid Email format"),
     password: Yup.string().required("Password is required"),
   });
 
-  const onSubmit = (values, onSubmitProps) => {
+  const onSubmit = (values) => {
     // console.log("form state", values);
     dispatch(actions.Login(values));
-    onSubmitProps.setSubmitting(false);
   };
-
+  const { handleSubmit, control, errors } = useForm({
+    mode: "onSubmit",
+    defaultValues: defaultValues,
+    resolver: yupResolver(schema),
+  });
   return (
     <>
       <ToastContainer limit={2}></ToastContainer>
       <h1>Login</h1>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-      >
-        {(formik) => {
-          return (
-            <>
-              <Form>
-                <FormikControl
-                  control="material-ui-input"
-                  type="email"
-                  name="email"
-                  label="Email"
-                  autoComplete="off"
-                />
-                <FormikControl
-                  control="material-ui-input"
-                  type="password"
-                  name="password"
-                  label="Password"
-                  autoComplete="off"
-                />
-                <CreateButton
-                  variant="contained"
-                  type="submit"
-                  text="Login"
-                  // background={theme.palette.primary.main}
-                  color="secondary"
-                  disabled={!formik.isValid || formik.isSubmitting}
-                />
-              </Form>
-              <div>
-                If you didn't have an account{" "}
-                <Link to="/register"> Register</Link>
-                <button onClick={() => forgetPassword(formik.values)}>
-                  forget password
-                </button>
-              </div>
-            </>
-          );
-        }}
-      </Formik>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          type="email"
+          name="email"
+          label="email"
+          control={control}
+          errors={errors}
+        />
+        <Input
+          type="password"
+          name="password"
+          label="password"
+          control={control}
+          errors={errors}
+        />
+        <CreateButton
+          variant="contained"
+          type="submit"
+          text="Login"
+          color="secondary"
+        />
+      </form>
+      <div>
+        If you didn't have an account <Link to="/register"> Register</Link>
+      </div>
     </>
   );
 }
