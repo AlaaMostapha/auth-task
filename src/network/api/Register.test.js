12544b5085aeb520320ef3db11d0b49c40f44300
AlaaMostapha <alaamoustapha21@gmail.com>
2020-12-28 16:28:53 +0200

    handle register test

--- src/network/api/Register.test.js
+++ src/network/api/Register.test.js
@@ -1,6 +1,7 @@
 import "regenerator-runtime/runtime";
 import mockAxios from "axios";
-const postRegister = require("./Register");
+// const postRegister = require("./Register");
+import { postRegister } from "./Register";
 // const axiosInstance = require("../axios/Register");
 // jest.mock("axiosInstance");
 
@@ -19,12 +20,17 @@ it("check register", async () => {
   });
   const result = await postRegister(data);
   const response = {
-    data: {
-      errors: [
-        { type: "email", error: "The email has already been taken" },
-        { type: "mobile_number", error: "The mobile number is invalid" },
-      ],
-    },
+    data: [
+      {
+        data: {
+          name: "alaa",
+          email: "alaa.moustafa@ibtikar.net.sa",
+          password: "12345678",
+          password_confirmation: "12345678",
+          mobile_number: "01150550943",
+        },
+      },
+    ],
   };
   expect(result).toEqual(response);
 });
