76a689e41f93265788f7d247762f0853fbd2f54f
AlaaMostapha <alaamoustapha21@gmail.com>
2020-12-30 14:41:53 +0200

    remove console logs

--- src/network/handlers/login.js
+++ src/network/handlers/login.js
@@ -8,16 +8,12 @@ export const handleRequest = (request) => {
 };
 export const handleResponse = (response) => {
   console.log("handle response", response);
-  // toast.success("Success !", {
-  //   position: toast.POSITION.TOP_CENTER,
-  // });
   history.push("home");
   return response;
 };
 export const handleError = (error) => {
   console.log("handle error", error);
   console.log(error.response);
-  // const errors = error.response.data.errors;
   toast.error("your Email or Password is incorret :(", {
     position: toast.POSITION.TOP_CENTER,
   });
